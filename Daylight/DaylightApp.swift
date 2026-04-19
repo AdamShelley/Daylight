a62aa141de2f205aa3919323bdf765e644c13060
Adam Shelley <adam.shelley18@gmail.com>
2026-04-19 16:52:33 +0800

    Initial Commit

---
+++ Daylight/DaylightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaylightApp.swift
+//  Daylight
+//
+//  Created by Adam Shelley on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DaylightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DaylightTests/DaylightTests.swift
@@ -0,0 +1,19 @@
+//
+//  DaylightTests.swift
+//  DaylightTests
+//
+//  Created by Adam Shelley on 19/04/2026.
+//
+
+import Testing
+@testable import Daylight
+
+struct DaylightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
